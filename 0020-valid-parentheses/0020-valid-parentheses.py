4ec2913b04838c673d0176d5c15ba4fd0c18c499
mohdinzamam09 <mohdinzi80@gmail.com>
2026-06-22 13:59:48 +0530

    Time: 3 ms (74.38%), Space: 12.6 MB (37.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for ch in s:
+            if ch == '(':
+                stack.append(')')
+            elif ch == '{':
+                stack.append('}')
+            elif ch == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != ch:
+                return False
+
+        return not stack
